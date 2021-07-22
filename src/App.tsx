85eb27cbd86e2b10146a3f71baf83d7a584a6267
Marco Agostinelli <51815533+marcoMilkman@users.noreply.github.com>
2021-07-22 18:12:28 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+const App = () => (
+	<div>ciao</div>
+)
+
+export default App;
---
+++ webpack/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = env => {
+	return {
+		entry: path.resolve(__dirname, '..', './src/index.tsx'),
+		resolve: {
+			extensions: ['.tsx', '.ts', '.js']
+		},
+		module: {
+			rules: [
+				{
+					test: /\.(ts|js)x?$/,
+					exclude: /node_modules/,
+					use: [
+						{
+							loader: 'babel-loader'
+						}
+					]
+				},
+			]
+		},
+		output: {
+			path: path.resolve(__dirname, '..', './build'),
+			filename: 'bundle.js'
+		},
+		mode: 'development',
+		plugins: [
+			new HtmlWebpackPlugin({
+				template: path.resolve(__dirname, '..', './public/index.html')
+			}),
+		]
+	}
+}

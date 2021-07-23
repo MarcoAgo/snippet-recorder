d2588a8c68d3c44c962469fc4e146db45270d578
Marco Agostinelli <carmelobig@icloud.com>
2021-07-23 17:52:22 +0200

    passed typed prop

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
-const App = () => {
+type appProps = {
+	testString: string,
+}
+
+const App = (props: appProps) => {
+	const { testString } = props;
 	return (
-		<h3>Welcome to your snippet collection</h3>
+		<h3>Welcome to your snippet collection, test: { testString }</h3>
 	)
 }
 
